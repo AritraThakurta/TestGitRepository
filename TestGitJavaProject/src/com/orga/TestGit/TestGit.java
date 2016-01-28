bf97683dbb9195bca213e16235a2a687d8c5a2db
Aritra Thakurta <aritra.thakurta89@gmail.com>
2016-01-28 23:06:03 +0530

    Initial Commit

---
+++ TestGitJavaProject/src/com/orga/TestGit/TestGit.java
@@ -0,0 +1,12 @@
+package com.orga.TestGit;
+
+public class TestGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Testing Git...");
+		
+
+	}
+
+}
